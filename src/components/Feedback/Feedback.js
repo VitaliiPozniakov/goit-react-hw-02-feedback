ab9567e08f12ba5615781b9556a1af41a1bec04d
VitaliiPozniakov <vertalen89@gmail.com>
2022-06-27 13:02:08 +0300

    1

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { PropTypes } from 'prop-types';
+// import { PropTypes } from 'prop-types';
 import { Title, Btn, Text, Value, BtnWraper} from './Feedback.styled'
 
 class Feedback extends Component {
